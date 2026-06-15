24845c78c8d33435b2edf2e918a73efd2d06cd71
InfinityCoding <infinitycoding@web.de>
2026-06-15 21:07:40 +0000

    Add helpers for creating data objects

--- betting_system/TurnierManager.cs
+++ betting_system/TurnierManager.cs
@@ -7,6 +7,40 @@ public class TurnierManager {
     private List<Benutzer> users = new();
     private List<Wette> bets = new();
 
+
+    // Helper methods to create objects
+    private Gruppe CreateGroup(string name, params string[] teamNames) {
+        var gruppe = new Gruppe(name);
+        foreach (var tName in teamNames) {
+            // Reuse existing team if already added (teams can appear in multiple groups in demo)
+            var team = teams.FirstOrDefault(t => t.Name == tName) ?? new Mannschaft(tName);
+            if (!teams.Contains(team))
+                teams.Add(team);
+            gruppe.AddTeam(team);
+        }
+        return gruppe;
+    }
+
+    private void AddMatch(ref int id, DateTime date, string time, string homeTeamName, string awayTeamName, Gruppe gruppe) {
+        var home = GetOrCreateTeam(homeTeamName);
+        var away = GetOrCreateTeam(awayTeamName);
+        var uhrzeit = TimeOnly.ParseExact(time, "HH:mm");
+        var spiel = new Spiel(id++, home, away, date, uhrzeit);
+        games.Add(spiel);
+        _ = gruppe; // gruppe is used for context; matches are stored globally
+    }
+
+    private Mannschaft GetOrCreateTeam(string name) {
+        var team = teams.FirstOrDefault(t => t.Name == name);
+        if (team == null) {
+            team = new Mannschaft(name);
+            teams.Add(team);
+        }
+        return team;
+    }
+
+    // end helpers
+
     public void initializeTurnier() {}
 
     public void saveAllData(string filename) {}
